6e152729af237a1650536b97e2baa01b8d0d87fb
edmilson-dk <droidvidaboa@gmail.com>
2021-06-04 11:46:01 -0300

    feat: created transform stream example-02

---
+++ node-streams/node-js-streams-everything-you-need-to-know/transform-stream/example-02.js
@@ -0,0 +1,39 @@
+const { Transform } = require("stream");
+
+const commaSplitter = new Transform({
+  readableObjectMode: true,
+
+  transform(chunk, encoding, callback) {
+    this.push(chunk.toString().trim().split(","));
+    callback();
+  }
+});
+
+const arrayToObject = new Transform({
+  writableObjectMode: true,
+  readableObjectMode: true,
+
+  transform(chunk, encoding, callback) {
+    const obj = {};
+    for(let i=0; i < chunk.length; i+=2) {
+      obj[chunk[i]] = chunk[i+1];
+    }
+    this.push(obj);
+    callback();
+  }
+});
+
+const objectToString = new Transform({
+  writableObjectMode: true,
+  
+  transform(chunk, encoding, callback) {
+    this.push(JSON.stringify(chunk) + '\n');
+    callback();
+  }
+});
+
+process.stdin
+  .pipe(commaSplitter) // []
+  .pipe(arrayToObject) // {}
+  .pipe(objectToString) // ""
+  .pipe(process.stdout);
